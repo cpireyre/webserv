f728898127bf2362e578394fc70851ab9a981d0f
uygarpolat <uygarpolat@gmail.com>
2025-03-31 10:37:27 +0300

    started CgiHandler

---
+++ include/CgiHandler.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+# include <string>
+
+class CgiHandler
+{
+	private:
+		std::string _contentLength;
+		std::string _contentType;
+		std::string _gatewayInterface;
+		std::string _pathInfo;
+		std::string _queryString;
+		std::string _remoteAddr;
+		std::string _requestMethod;
+		std::string	_scriptName;
+		std::string	_serverName;
+		std::string	_serverPort;
+		std::string _serverProtocol;
+}
---
+++ src/CgiHandler.cpp
@@ -0,0 +1,2 @@
+#include "../include/CgiHandler.hpp"
+
