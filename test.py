77689197b631386b9ebf208ff45dce6205195818
uygarpolat <uygarpolat@gmail.com>
2025-04-10 15:57:04 +0300

    some unit tests removed

--- test.py
+++ test.py
@@ -13,4 +13,3 @@ def test_get_root():
     response = requests.get("http://127.0.0.1:8080/index.html")
     # Check that status code, content, etc. match your expectations.
     assert response.status_code == 200
-    assert response.content == None
