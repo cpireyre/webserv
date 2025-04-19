d466600e5bdc806bb187d7f5ff1a53726e48b970
Colin <cpireyre@student.42.fr>
2025-04-19 14:01:08 +0300

    Zero out IP and port when disconnecting client

--- src/Client.cpp
+++ src/Client.cpp
@@ -63,10 +63,11 @@ void	disconnectClient(Endpoint *client, int qfd)
 	close(client->handler.getClientSocket());
 	client->state = CONNECTION_DISCONNECTED;
 	client->sockfd = -1;
+	bzero(client->IP, INET6_ADDRSTRLEN);
+	bzero(client->port, PORT_STRLEN);
 	client->error = 0;
 	client->began_sending_header_ms = 0;
 	client->last_heard_from_ms = 0;
 	client->handler.setClientSocket(-1);
 	client->handler.resetObject();
 }
-
